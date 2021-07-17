7452825ddccb587b025a0899492044ab098c4ef1
David Figueroa <figueroada@vcu.edu>
2021-07-17 13:21:12 -0400

    added connection to mongodb atlas

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+//Notes: 
+//We will add in MONGODB_URI in the .env file from Mark's MongoDB Atlas connection and on Heroku. 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/traderjocks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
