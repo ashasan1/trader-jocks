36d1c6bdb2980095ab1f14e2836be3d5ef617781
Justin Byrd <justin_byrd@hotmail.com>
2021-07-18 16:59:25 -0400

    added addUser to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,43 +1,51 @@
 const { League } = require('../models');
+const User = require('../models/User');
 
 const resolvers = {
-  Query: {
-    leagues: async () => {
-      return League.find().sort({ createdAt: -1 });
-    },
+	Query: {
+		leagues: async () => {
+			return League.find().sort({ createdAt: -1 });
+		},
 
-    league: async (parent, { leagueId }) => {
-      return League.findOne({ _id: leagueId });
-    },
-  },
+		league: async (parent, { leagueId }) => {
+			return League.findOne({ _id: leagueId });
+		},
+	},
 
-  Mutation: {
-    addLeague: async (parent, { leagueInitials, leagueName, leagueLogo }) => {
-      return League.create({ leagueInitials, leagueName, leagueLogo });
-    },
-    addTeam: async (parent, { leagueId, teamCity, teamName, teamLogo }) => {
-      return League.findOneAndUpdate(
-        { _id: leagueId },
-        {
-          $addToSet: { teams: { teamCity, teamName, teamLogo } },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-    },
-    removeLeague: async (parent, { leagueId }) => {
-      return League.findOneAndDelete({ _id: leagueId });
-    },
-    removeTeam: async (parent, { leagueId, teamId }) => {
-      return League.findOneAndUpdate(
-        { _id: leagueId },
-        { $pull: { teams: { _id: teamId } } },
-        { new: true }
-      );
-    },
-  },
+	Mutation: {
+		addLeague: async (parent, { leagueInitials, leagueName, leagueLogo }) => {
+			return League.create({ leagueInitials, leagueName, leagueLogo });
+		},
+		addTeam: async (parent, { leagueId, teamCity, teamName, teamLogo }) => {
+			return League.findOneAndUpdate(
+				{ _id: leagueId },
+				{
+					$addToSet: { teams: { teamCity, teamName, teamLogo } },
+				},
+				{
+					new: true,
+					runValidators: true,
+				}
+			);
+		},
+		removeLeague: async (parent, { leagueId }) => {
+			return League.findOneAndDelete({ _id: leagueId });
+		},
+		removeTeam: async (parent, { leagueId, teamId }) => {
+			return League.findOneAndUpdate(
+				{ _id: leagueId },
+				{ $pull: { teams: { _id: teamId } } },
+				{ new: true }
+			);
+		},
+
+		addUser: async (parent, { name, email, password }) => {
+			const newUser = await User.create({ username, email, password });
+			const token = signToken(newUser);
+			return { token, newUser };
+		}
+
+	},
 };
 
 module.exports = resolvers;
