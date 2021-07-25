4274ead59673e235f8541ab88860a3e0b95ece78
Justin Byrd <justin_byrd@hotmail.com>
2021-07-25 14:23:28 -0400

    added Login component

---
+++ client/src/components/Login.js
@@ -0,0 +1,32 @@
+import React, { userState } from 'react';
+import {Form, Button, Alert} from 'react-bootstrap';
+import {useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Login = () => {
+
+	return (
+		<>
+			<Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                Enter email associated with your Jocks Account
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+		</>
+	)
+}
