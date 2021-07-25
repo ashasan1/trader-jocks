71afcaea04739a2b75de34a5a3d2ed2e8a42ff3a
Justin Byrd <justin_byrd@hotmail.com>
2021-07-25 14:16:30 -0400

    updated Auth and added loginUser to server/schemas/typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -50,7 +50,7 @@ const typeDefs = gql`
   }
 
   type Auth {
-	  token: ID!
+	  token: String
 	  user: User
   }
 
@@ -63,7 +63,8 @@ const typeDefs = gql`
       playerSoundex: String): Item
     addLeague(leagueIntials: String!, leagueName: String!, leagueLogo: String): League
     addTeam(leagueId: ID!, teamCity: String!, teamName: String!, teamLogo: String!): League
-	  addUser(username: String!, password: String!, email: String!): Auth
+	 addUser(username: String!, password: String!, email: String!): Auth
+	 loginUser(username: String!, password: String!): Auth
     removeItem(itemId: ID!): Item
     removeLeague(leagueId: ID!): League
     removeTeam(leagueId: ID!, teamId: ID!): League
