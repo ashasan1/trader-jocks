9905e9625c266f4e9739668a498debbcb41459fe
Justin Byrd <justin_byrd@hotmail.com>
2021-07-25 13:29:23 -0400

    added client/src/utils/muations.js with addUser mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const SIGNUP = gql`
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		addUser(username: $username, email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`;
