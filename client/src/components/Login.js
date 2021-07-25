965c80a9d8d4a4c02e81e823717e479eea3f8c88
Justin Byrd <justin_byrd@hotmail.com>
2021-07-25 17:09:29 -0400

    added handleFormSubmit functionality to client/src/components/Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,27 +1,46 @@
-import React, { userState } from 'react';
+import React, { useState } from 'react';
 import {Form, Button, Alert} from 'react-bootstrap';
 import {useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Login = () => {
+	const [userFormData, setUserFormData] = useState({ username: '', password: ''});
+	const [login, { error }] = useMutation(LOGIN_USER);
 
+	const handleInputChange = (event) => {
+		const { name, value } = event.target;
+		setUserFormData({ ...userFormData, [name]: value});
+	}
+
+	const handleFormSubmit = async (event) => {
+		event.preventDefault();
+
+		const form = event.currentTarget;
+		if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+
+		try {
+			const { data } = await login({ variables: { ...userFormData }});
+			if (!data) throw new Error('something went wrong');
+			Auth.login(data.loginUser.token);
+		} catch (err) {
+			console.log(err);
+		}
+	}
 	return (
 		<>
-			<Form>
+			<Form onSubmit={handleFormSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                Enter email associated with your Jocks Account
-              </Form.Text>
-            </Form.Group>
+              <Form.Label>User Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter User Name" name="username" onChange={handleInputChange} />
+           </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Control type="password" placeholder="Password" name="password" onChange={handleInputChange} />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
