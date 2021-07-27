eba7c819a924e4e64f90270db3d25b49ce964237
Omari Grampus <ojgrampus@gmail.com>
2021-07-26 21:41:59 -0400

    added profile.js

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,125 @@
+import React from "react";
+// import Auth from '../utils/auth';
+import {
+  Carousel,
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  CardColumns,
+} from "react-bootstrap";
+import { Redirect } from "react-router-dom";
+import "../index.css";
+import axios from "axios";
+
+function UserProfile() {
+
+    // const GetItemData = {
+    //     query: {}
+    // }
+
+    
+  return (
+    <>
+      <div>
+        <h1>My Items</h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div>
+            <h3 className="searchheader">Items</h3>
+          </div>
+
+          <div id="searchitem">
+            <img
+              id="searchitemimage"
+              src="https://image.shutterstock.com/image-photo/soccer-ball-isolated-on-white-260nw-129557066.jpg"
+            ></img>
+            <p id="searchitemdescription">
+              {" "}
+              Micheal Jordan Basketball signed in 2006
+            </p>
+          </div>
+
+          <div id="searchitem">
+            <img
+              id="searchitemimage"
+              src="https://image.shutterstock.com/image-photo/soccer-ball-isolated-on-white-260nw-129557066.jpg"
+            ></img>
+            <p id="searchitemdescription">
+              {" "}
+              Micheal Jordan Basketball signed in 2006
+            </p>
+          </div>
+
+          <div id="searchitem">
+            <img
+              id="searchitemimage"
+              src="https://image.shutterstock.com/image-photo/soccer-ball-isolated-on-white-260nw-129557066.jpg"
+            ></img>
+            <p id="searchitemdescription">
+              {" "}
+              Micheal Jordan Basketball signed in 2006
+            </p>
+          </div>
+
+          <div id="searchitem">
+            <img
+              id="searchitemimage"
+              src="https://image.shutterstock.com/image-photo/soccer-ball-isolated-on-white-260nw-129557066.jpg"
+            ></img>
+            <p id="searchitemdescription">
+              {" "}
+              Micheal Jordan Basketball signed in 2006
+            </p>
+          </div>
+
+          <div id="searchitem">
+            <img
+              id="searchitemimage"
+              src="https://image.shutterstock.com/image-photo/soccer-ball-isolated-on-white-260nw-129557066.jpg"
+            ></img>
+            <p id="searchitemdescription">
+              {" "}
+              Micheal Jordan Basketball signed in 2006
+            </p>
+          </div>
+          <div id="searchitem">
+            <img
+              id="searchitemimage"
+              src="https://image.shutterstock.com/image-photo/soccer-ball-isolated-on-white-260nw-129557066.jpg"
+            ></img>
+            <p id="searchitemdescription">
+              {" "}
+              Micheal Jordan Basketball signed in 2006
+            </p>
+          </div>
+          <div id="searchitem">
+            <img
+              id="searchitemimage"
+              src="https://image.shutterstock.com/image-photo/soccer-ball-isolated-on-white-260nw-129557066.jpg"
+            ></img>
+            <p id="searchitemdescription">
+              {" "}
+              Micheal Jordan Basketball signed in 2006
+            </p>
+          </div>
+          <div id="searchitem">
+            <img
+              id="searchitemimage"
+              src="https://image.shutterstock.com/image-photo/soccer-ball-isolated-on-white-260nw-129557066.jpg"
+            ></img>
+            <p id="searchitemdescription">
+              {" "}
+              Micheal Jordan Basketball signed in 2006
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UserProfile;
