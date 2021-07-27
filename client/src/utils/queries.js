76354507d854c5b6f1662d9cea7d4efc16070e04
MarkHatTHORinc <markh@thorinc.com>
2021-07-26 22:25:08 -0400

    add queries for User search and items by player soundex

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,5 @@
 import gql from 'graphql-tag';
+
  export const CAROUSEL_ITEMS = gql`
 	{
 		getCarouselItems {
@@ -6,4 +7,42 @@ import gql from 'graphql-tag';
 			imageURL
 		}
 	}
+`;
+
+	export const USER_ITEMS = gql`
+	{
+		query getUserItems {
+			user {
+			_id
+			username
+			items {
+				# _id
+				title
+				description
+				imageURL
+				price
+				playerName
+				playerSoundex
+			}
+			createdAt
+			}
+		}
+	}
  `;
+
+
+ export const ITEMS_SOUNDEX = gql`
+ {
+	 query getItemsByPlayerSoundex {
+		 items {
+			 # _id
+			 title
+			 description
+			 imageURL
+			 price
+			 playerName
+			 playerSoundex
+		 }
+	 }
+ }
+`; 
