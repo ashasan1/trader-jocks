4745172bd30191b7b51f22e752de9144bdd9cca3
David Figueroa <figueroada@vcu.edu>
2021-07-28 22:48:48 -0400

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //Notes: 
 //We will add in MONGODB_URI in the .env file from Mark's MongoDB Atlas connection and on Heroku. 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://figueroada:jonathan@fitnesstrackerdf.ukib7.mongodb.net/traderjocks?retryWrites=true&w=majority', { 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/traderjocks', { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
