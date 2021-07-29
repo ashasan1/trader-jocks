5419803d96ffc6dc1f79f608f051f2430b03ac6d
Justin Byrd <justin_byrd@hotmail.com>
2021-07-29 16:13:06 -0400

    updated SearchResults to display results

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,16 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/client';
 // import Auth from '../utils/auth';
 import { Carousel, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import '../index.css'
-import ItemCarousel from '../components/ItemCarousel';
-import { GET_ITEMS } from '../utils/queries';
+import { SEARCH_ITEMS } from '../utils/queries';
 
 const SearchResults = (searchTerm) => {
+	const { loading, data } = useQuery(SEARCH_ITEMS);
 
-return (
-<h2>I'm HERE!! and the search criteria is ${searchTerm}</h2>
-)
-}
+	if (loading) {
+		return <h2>LOADING...</h2>
+	}
 
-export default SearchResults
+	return (
+		<>
+			<div className="container">
+				<div className="row">
+
+					<div>
+						<h3 className="searchheader">MLB Items</h3>
+					</div>
+
+					{data.getSearchItems.map((searchItem) => {
+						return (
+							<div id="searchitem">
+								<img id="searchitemimage" src="https://image.shutterstock.com/image-photo/soccer-ball-isolated-on-white-260nw-129557066.jpg"></img>
+								<p id="searchitemdescription"> Micheal Jordan Basketball signed in 2006</p>
+							</div>
+							)
+						}
+					)}
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default SearchResults;
