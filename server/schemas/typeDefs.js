6ec5b4f1ace2da11ed3aa2b740fc538bb5bc40cf
Justin Byrd <justin_byrd@hotmail.com>
2021-07-29 13:59:22 -0400

    added getSearchItems to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -40,6 +40,7 @@ const typeDefs = gql`
   type Query {
     categories: [Category]!
     items: [Item]!
+	 getSearchItems: [Item]!
     leagues: [League]!
     league(leagueId: ID!): League
     leagueByInitials(initials: String!): League
